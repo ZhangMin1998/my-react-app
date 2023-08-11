60cb4a508f996904c28e1c227dfdb3161034d3e2
ZhangMin1998 <1464253423@qq.com>
2023-08-12 01:49:33 +0800

    JSX中使用js表达式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+
+const name = '张敏'
+const getAge = () => {
+  return 25
+}
+const isTrue = true
+
 function App() {
   return (
     <div className="App">
-      app
+      <h1>你好，我叫{name}</h1>
+      <h2>今年{getAge()}岁</h2>
+      <h3>{isTrue ? '真帅' : '666'}</h3>
     </div>
-  );
+  )
 }
 
 export default App;
