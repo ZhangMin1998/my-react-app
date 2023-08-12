b1530b6310a96515a86369c8f9644af2f9f211c4
ZhangMin1998 <1464253423@qq.com>
2023-08-12 13:52:13 +0800

    注意事项

--- src/04.JSX样式处理.jsx
+++ src/04.JSX样式处理.jsx
@@ -1,22 +1,22 @@
 // JSX样式处理
-// 行内样式 - style 
+// 行内样式 - style
 // 类名 - className（推荐）
 import './app.css'
 
 const obj = {
   color: 'blue',
-  fontSize: '26px'
+  fontSize: '26px',
 }
 const showTitle = true
 
 function App() {
   return (
     <div className="App">
-      <div style={{color: 'red',fontSize:'30px'}}> this is zhangmin</div>
+      <div style={{ color: 'red', fontSize: '30px' }}> this is zhangmin</div>
       <div style={obj}> I'm 25 years old</div>
       <div className={showTitle ? 'title' : ''}>我是中国人</div>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-// JSX样式处理
-// 行内样式 - style 
-// 类名 - className（推荐）
-import './app.css'
+// JSX注意事项
 
-const obj = {
-  color: 'blue',
-  fontSize: '26px'
-}
-const showTitle = true
+// JSX必须有一个根节点，如果没有根节点，可以使用<></>（幽灵节点）替代
+// 所有标签必须形成闭合，成对闭合或者自闭合都可以
+// JSX中的语法更加贴近JS语法，属性名采用驼峰命名法  class -> className  for -> htmlFor
+// JSX支持多行（换行），如果需要换行，需使用() 包裹，防止bug出现
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <div style={{color: 'red',fontSize:'30px'}}> this is zhangmin</div>
-      <div style={obj}> I'm 25 years old</div>
-      <div className={showTitle ? 'title' : ''}>我是中国人</div>
-    </div>
+    <>
+      <div className="App"></div>
+      <div className="App"></div>
+    </>
   )
 }
 
-export default App;
+export default App
