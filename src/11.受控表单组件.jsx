24898ab08fcd101164d4943c8a2a88a95943904a
ZhangMin1998 <1464253423@qq.com>
2023-08-13 17:16:39 +0800

    非受控表单组件

--- src/11.受控表单组件.jsx
+++ src/11.受控表单组件.jsx
@@ -33,3 +33,8 @@ export default App
 
 
 // 受控组件：input框自己的状态被React组件状态控制
+
+// 1. 在组件的state中声明一个组件的状态数据
+// 2. 将状态数据设置为input标签元素的value属性的值
+// 3. 为input添加change事件，在事件处理程序中，通过事件对象e获取到当前文本框的值（即用户当前输入的值）
+// 4. 调用setState方法，将文本框的值作为state状态的最新值
