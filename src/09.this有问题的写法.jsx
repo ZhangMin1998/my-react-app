d4b4128236a918a9705c46f62303c8730eb3b917
ZhangMin1998 <1464253423@qq.com>
2023-08-13 16:23:22 +0800

    this指向问题 3种写法

--- src/09.this有问题的写法.jsx
+++ src/09.this有问题的写法.jsx
@@ -2,15 +2,44 @@
 import React from 'react'
 
 // 类组件 this有问题的写法
+
+// 1.constructor中通过bind强行绑定this
+
+// class HelloComponent extends React.Component{
+//   constructor () {
+//     super()
+//     // 使用bind强行修改this指向
+//     // 相当于在类组件初始化的阶段，就可以把回调函数的this修改到
+//     // 永远指向当前组件的实例对象
+//     this.clickHandler = this.clickHandler.bind(this)
+//   }
+//   clickHandler () {
+//     console.log(this)
+//   }
+//   render () {
+//     return <div onClick={this.clickHandler} style={{fontSize: '30px',color: 'red'}}>这是我第一个类组件</div>
+//   }
+// }
+
+// 2.箭头函数写法
+
+// class HelloComponent extends React.Component{
+//   clickHandler () {
+//     console.log(this)
+//   }
+//   render () {
+//     // render函数的this已经被react做了修改
+//     // 这里的this就是指向当前组件实例
+//     console.log('父函数中的this指向为：', this)
+//     // 通过箭头函数的写法 直接沿用父函数的this指向也ok
+//     return <div onClick={() => this.clickHandler()} style={{fontSize: '30px',color: 'red'}}>这是我第一个类组件</div>
+//   }
+// }
+
+
+// 3. class field写法  最推荐
 class HelloComponent extends React.Component{
-  constructor () {
-    super()
-    // 使用bind强行修改this指向
-    // 相当于在类组件初始化的阶段，就可以把回调函数的this修改到
-    // 永远指向当前组件的实例对象
-    this.clickHandler = this.clickHandler.bind(this)
-  }
-  clickHandler () {
+  clickHandler = () => {
     console.log(this)
   }
   render () {
--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 // 类组件引入React
 import React from 'react'
 
-// 组件状态
-// 不要直接修改state中的值，必须通过setState方法进行修改 
-class TestComponent extends React.Component{
-  // 定义组件状态
-  state = {
-    name: 'zm',
-    count: 0
-  }
-  setCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-      name: 'zzzz'
-    })
+// 类组件 this有问题的写法
+
+// 1.constructor中通过bind强行绑定this
+
+// class HelloComponent extends React.Component{
+//   constructor () {
+//     super()
+//     // 使用bind强行修改this指向
+//     // 相当于在类组件初始化的阶段，就可以把回调函数的this修改到
+//     // 永远指向当前组件的实例对象
+//     this.clickHandler = this.clickHandler.bind(this)
+//   }
+//   clickHandler () {
+//     console.log(this)
+//   }
+//   render () {
+//     return <div onClick={this.clickHandler} style={{fontSize: '30px',color: 'red'}}>这是我第一个类组件</div>
+//   }
+// }
+
+// 2.箭头函数写法
+
+// class HelloComponent extends React.Component{
+//   clickHandler () {
+//     console.log(this)
+//   }
+//   render () {
+//     // render函数的this已经被react做了修改
+//     // 这里的this就是指向当前组件实例
+//     console.log('父函数中的this指向为：', this)
+//     // 通过箭头函数的写法 直接沿用父函数的this指向也ok
+//     return <div onClick={() => this.clickHandler()} style={{fontSize: '30px',color: 'red'}}>这是我第一个类组件</div>
+//   }
+// }
+
+
+// 3. class field写法  最推荐
+class HelloComponent extends React.Component{
+  clickHandler = () => {
+    console.log(this)
   }
   render () {
-    // 使用状态
-    return (
-      <div>
-        this is TestComponent, name为: {this.state.name}, count: {this.state.count}
-        <button onClick={this.setCount}>click++</button>
-      </div>
-    )
+    return <div onClick={this.clickHandler} style={{fontSize: '30px',color: 'red'}}>这是我第一个类组件</div>
   }
 }
 
 function App () {
   return (
     <div className="App">
-      <TestComponent/>
+      <HelloComponent/>
     </div>
   )
 }
