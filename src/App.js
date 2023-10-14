5f4a623c960708e70bb8a30294336a29bb83435d
ZhangMin1998 <1464253423@qq.com>
2023-10-14 14:01:31 +0800

    RenderProps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 // import SCUDemo from './6.高级特性/1.SCUDemo'
 // import SCUDemo2 from './6.高级特性/2.SCUDemo2'
 // import PureComponentDemo from './6.高级特性/3.PureComponentDemo'
-import HOCDemo from './6.高级特性/4.HOCDemo'
+// import HOCDemo from './6.高级特性/4.HOCDemo'
+import RenderPropDemo from './6.高级特性/6.RenderProp'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <SCUDemo/> */}
       {/* <SCUDemo2 /> */}
       {/* <PureComponentDemo /> */}
-      <HOCDemo a='999' />
+      {/* <HOCDemo a='999' /> */}
+      <RenderPropDemo a='666' />
     </div>
   )
 }
